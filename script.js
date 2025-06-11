648066c0c4cef8bb87f36b1bee43c8c6549e66d6
Harshithadale <daleharshi6045@gmail.com>
2025-06-11 22:07:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 const age = document.getElementById("age");
 const name = document.getElementById("name");
-document.getElementById("btn").onclick = (e)=>{
-	e.preventDefault()
-	if(age.value == "" || name.value == ""){
-		window.alert("Please enter valid details")
-		return 
+
+document.getElementById("btn").onclick = (e) => {
+	e.preventDefault();
+
+	if (age.value === "" || name.value === "") {
+		window.alert("Please enter valid details");
+		return;
 	}
-	const myPromise = new Promise((res,rej) =>{
-		if(age.value >= 18){
-			setTimeout(()=>{
-				res(`Welcome, ${name.value}. You can vote`)
-			,4000})
+
+	const myPromise = new Promise((res, rej) => {
+		if (Number(age.value) >= 18) {
+			setTimeout(() => {
+				res(`Welcome, ${name.value}. You can vote`);
+			}, 4000);
+		} else {
+			setTimeout(() => {
+				rej(`Oh sorry ${name.value}. You aren't old enough`);
+			}, 4000);
 		}
-		else{
-			setTimeout(()=>{
-				rej(`Oh sorry ${name.value}. You aren't old enough`)
-			},4000)
-		}
-	})
-	myPromise.then(data=>{
-	window.alert(data)
-	})
-	.catch(err=>{
-		window.alert(err)
-	})
+	});
+
+	myPromise
+		.then(data => {
+			window.alert(data);
+		})
+		.catch(err => {
+			window.alert(err);
+		});
+};
+
 }
