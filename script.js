ec24c66ad9af4696d6f469270a58cb0e3e06902b
Harshithadale <daleharshi6045@gmail.com>
2025-06-11 21:59:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const name = document.getElementById("name");
 document.getElementById("btn").onclick = (e)=>{
 	e.preventDefault()
 	if(age.value == "" || name.value == ""){
-		window.alert("Please enter valid details.")
+		window.alert("Please enter valid details")
 		return 
 	}
 	const myPromise = new Promise((res,rej) =>{
 		if(age.value >= 18){
 			setTimeout(()=>{
-				res(`Welcome, ${name}. You can vote.`)
+				res(`Welcome, ${name}. You can vote`)
 			,4000})
 		}
 		else{
 			setTimeout(()=>{
-				rej(`Oh sorry ${name}. You aren't old enough.`)
+				rej(`Oh sorry ${name}. You aren't old enough`)
 			},4000)
 		}
 	})
